e1b68ecbf6684c7ab3ec108bf6f93e0a778c8126
gcadigwe@gmail.com <gcadigwe@gmail.com>
2024-11-16 12:51:20 +0100

    thread creation, mutex

--- main.cpp
+++ main.cpp
@@ -7,24 +7,41 @@
 
 #include <iostream>
 #include <queue>
+#include <thread>
 
-int add(int a, int b) {
-    return a + b;
+std::mutex mtx;
+
+void getDataAndExec (std::queue<int>& queue, int id) {
+    std::lock_guard<std::mutex> lock(mtx);
+    
+    std::cout << "Thread ID: " << id << " Queue Data: " << queue.front() << std::endl;
+
+    queue.pop();
 }
 
-int subtract(int a, int b) {
-    return a - b;
-}
 
 int main(int argc, const char * argv[]) {
-    std::queue<std::function<int()>> funcQueue;
-    funcQueue.push(std::bind(add, 10, 3));
-    funcQueue.push(std::bind(subtract, 7, 4));
+    std::queue<int> queue;
+    std::vector<std::thread> threads;
     
-    while (!funcQueue.empty()) {
-        int result = funcQueue.front()();
-        std::cout << "Result: " << result << std::endl;
-        funcQueue.pop();
+    queue.push(2);
+    queue.push(3);
+    queue.push(4);
+    queue.push(5);
+    queue.push(6);
+    
+    long size = queue.size();
+    
+    std::cout << "queue size: " << size << std::endl;
+    
+    for(int i = 0; i < size; i++) {
+        threads.emplace_back(getDataAndExec,std::ref(queue), i);
+//        std::cout << "Created Thread with ID: " << threads.back().get_id() << std::endl;
+    }
+    
+    for (auto& t: threads) {
+        std::cout << "Original Thread id: " << t.get_id() << std::endl;
+        t.join();
     }
   
     return 0;
